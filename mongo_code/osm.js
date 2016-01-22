bd2d01fbfa3a9c3410ed9948e836519cd576cb6a
ashic <ashic@live.com>
2016-01-22 03:51:03 +0000

    ready

---
+++ mongo_code/osm.js
@@ -0,0 +1,47 @@
+conn = new Mongo();
+db = conn.getDB("osm");
+
+var cuisine_entries = db.raw1.find({'cuisine': {$exists:1}}).length();
+print(cuisine_entries);
+
+var cuisines = db.raw1.aggregate([
+		{$match: {'cuisine': {$exists:1}}},
+		{$group: { _id: "$cuisine", 'count': {$sum:1} } }
+	]);
+
+
+cuisines.forEach(printjson);
+
+var cleanup_cuisine_name = function(c){
+	var lowered = c.toLowerCase();
+	var split = lowered.split(/[,;&]+/)
+	for(var s in split){
+		split[s] = split[s].trim()
+		if(split[s] && split[s][0] == '_') split[s] = split[s].substring(1);
+		if(split[s] && split[s][split[s].length-1] == '_') 
+			split[s] = split[s].substring(0, split[s].length-1);
+		if(split[s] == "british_food") split[s] = "british";
+	}
+
+	return split;
+}
+
+db.createCollection("cuisines");
+db.cuisines.createIndex({cuisine:1});
+
+db.raw1.find({'cuisine': {$exists:1}}).snapshot().forEach(function(el){
+ 	el.cuisine = cleanup_cuisine_name(el.cuisine);
+ 	db.cuisines.save(el);
+});
+
+
+var popular_cuisines = db.cuisines.aggregate([
+		{$match: {'cuisine': {$exists:1}}},					//only entries with cuisines
+		{$group: { _id: "$cuisine", 'count': {$sum:1} } },	//one for each array
+		{$unwind: "$_id"},									//arrays unwinded
+		{$group: {_id:"$_id", count: {$sum: "$count"}}},	// grouped count after unwinding
+		{$sort: {count: -1}},								
+		{$out: "popular_cuisines"}							//store to result collection for easy querying
+	]);
+
+popular_cuisines.forEach(printjson);
